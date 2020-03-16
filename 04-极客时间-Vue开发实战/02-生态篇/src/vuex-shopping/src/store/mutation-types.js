a358f1eab9233351c1aece052a72e09a5604add0
guqianfeng <guqianfeng@angelalign.com>
2020-03-16 14:54:20 +0800

    mutation-types增加注释

--- 04-极客时间-Vue开发实战/02-生态篇/src/vuex-shopping/src/store/mutation-types.js
+++ 04-极客时间-Vue开发实战/02-生态篇/src/vuex-shopping/src/store/mutation-types.js
@@ -1,11 +1,11 @@
 export const CART = {
-  PUSH_PRODUCT_TO_CART: 'pushProductToCart',
-  INCREMENT_ITEM_QUANTITY: 'incrementItemQuantity',
-  SET_CART_ITEMS: 'setCartItems',
-  SET_CHECKOUT_STATUS: 'setCheckoutStatus'
+  PUSH_PRODUCT_TO_CART: 'pushProductToCart', //  把商品加到购物车 - 购物车没同样商品的时候，添加，数量为1
+  INCREMENT_ITEM_QUANTITY: 'incrementItemQuantity', //  把商品加到购物车 - 购物车有同样商品的时候数量加1
+  SET_CART_ITEMS: 'setCartItems', //  购物车商品列表
+  SET_CHECKOUT_STATUS: 'setCheckoutStatus' //  购买提交后状态 - 是否购买成功
 }
 
 export const PRODUCTS = {
-  SET_PRODUCTS: 'setProducts',
-  DECREMENT_PRODUCT_INVENTORY: 'decrementProductInventory'
+  SET_PRODUCTS: 'setProducts', //  获取商品列表
+  DECREMENT_PRODUCT_INVENTORY: 'decrementProductInventory' // 点击添加购物车后减库存
 }
