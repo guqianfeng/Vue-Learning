550ba6c76ce8e2d6ca87319094de51ac10bc7cdb
qianfengg <fred_gqf0221@163.com>
2020-03-27 23:45:55 +0800

    组件中使用第三方库

--- 04-极客时间-Vue开发实战/03-实战篇/src/ant-design-vue-pro/src/components/Chart.vue
+++ 04-极客时间-Vue开发实战/03-实战篇/src/ant-design-vue-pro/src/components/Chart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="chartDom" style="height: 500px"></div>
+  <div ref="chartDom"></div>
 </template>
 
 <script>
@@ -7,40 +7,40 @@ import echarts from 'echarts'
 import { addListener, removeListener } from 'resize-detector'
 import debounce from 'lodash/debounce'
 export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    option (val) {
+      this.chart.setOption(val);
+    }
+    // option: {
+    //   handler (val) {
+    //     this.chart.setOption(val)
+    //   },
+    //   deep: true
+    // }
+  },
   created () {
     this.resize = debounce(this.resize, 300)
   },
   mounted () {
-    // 基于准备好的dom，初始化echarts实例
-    this.chart = echarts.init(this.$refs.chartDom);
+    this.renderChart()
     addListener(this.$refs.chartDom, this.resize)
-    // 指定图表的配置项和数据
-    var option = {
-        title: {
-            text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-            data:['销量']
-        },
-        xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
-    };
-
-    // 使用刚指定的配置项和数据显示图表。
-    this.chart.setOption(option);
   },
   methods: {
     resize () {
       console.log('resize')
       this.chart.resize()
+    },
+    renderChart () {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(this.$refs.chartDom);
+      // 使用刚指定的配置项和数据显示图表。
+      this.chart.setOption(this.option);
     }
   },
   beforeDestory () {
--- 04-极客时间-Vue开发实战/03-实战篇/src/ant-design-vue-pro/src/views/Dashboard/Analysis.vue
+++ 04-极客时间-Vue开发实战/03-实战篇/src/ant-design-vue-pro/src/views/Dashboard/Analysis.vue
@@ -1,18 +1,51 @@
 <template>
   <div>
-    <Chart/>
+    <Chart :option="chartOption" style="height: 400px"/>
   </div>
 </template>
 
 <script>
-import Chart from '../../components/Chart'
+import Chart from "../../components/Chart"
+import random from 'lodash/random'
 export default {
   components: {
     Chart
+  },
+  mounted () {
+    this.interval = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(() => random(100))
+      this.chartOption = { ...this.chartOption } //不使用深度监听时手动更改
+    }, 2000)
+  },
+  beforeDestory () {
+    clearInterval(this.interval)
+  },
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
