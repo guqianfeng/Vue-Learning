b6032eaefcbb64ebf6ffffe7620475c783f7407c
qianfengg <fred_gqf0221@163.com>
2020-05-17 18:33:16 +0800

    后端模型定义

--- trello/backend/src/app.ts
+++ trello/backend/src/app.ts
@@ -1,57 +1,61 @@
-import configs from './configs'
-import Koa, { Context } from 'koa'
-import { bootstrapControllers } from 'koa-ts-controllers'
-import KoaRouter from 'koa-router'
-import KoaBodyParser from 'koa-bodyparser'
-import path from 'path'
-import Boom from '@hapi/Boom'
+import configs from "./configs";
+import Koa, { Context } from "koa";
+import { bootstrapControllers } from "koa-ts-controllers";
+import KoaRouter from "koa-router";
+import KoaBodyParser from "koa-bodyparser";
+import path from "path";
+import Boom from "@hapi/Boom";
+import { Sequelize } from "sequelize-typescript";
 
 (async () => {
+  const app = new Koa();
 
-  const app = new Koa()
+  const router = new KoaRouter();
 
-  const router = new KoaRouter()
-  
+  // 链接数据库
+  const db = new Sequelize({
+    ...configs.database,
+    models: [__dirname + "/models/**/*"],
+  });
+
   // 注册路由
   await bootstrapControllers(app, {
     router,
-    basePath: '/api',
+    basePath: "/api",
     versions: [1],
-    controllers: [
-      path.resolve(__dirname + '/controllers/**/*')
-    ],
+    controllers: [path.resolve(__dirname + "/controllers/**/*")],
     errorHandler: async (err: any, ctx: Context) => {
-      console.log(err)
-      let status = 500
+      console.log(err);
+      let status = 500;
       let body: any = {
         statusCode: status,
-        error: 'Internal server error',
-        message: 'An internal server error occurred'
-      }
+        error: "Internal server error",
+        message: "An internal server error occurred",
+      };
 
       if (err.output) {
-        status = err.output.statusCode
-        body = {...err.output.payload}
+        status = err.output.statusCode;
+        body = { ...err.output.payload };
         if (err.data) {
-          body.errorDetails = err.data
+          body.errorDetails = err.data;
         }
       }
 
-      ctx.status = status
-      ctx.body = body
-    }
-  })
+      ctx.status = status;
+      ctx.body = body;
+    },
+  });
 
-  router.all('*', async ctx => {
-    throw Boom.notFound('不存在该路由')
-  })
+  router.all("*", async (ctx) => {
+    throw Boom.notFound("不存在该路由");
+  });
 
-  app.use(KoaBodyParser())
-  app.use(router.routes())
+  app.use(KoaBodyParser());
+  app.use(router.routes());
 
   app.listen(configs.server.port, configs.server.host, () => {
-    console.log(`服务启动成功: http://${configs.server.host}:${configs.server.port}`)
-  })
-
-})()
-
+    console.log(
+      `服务启动成功: http://${configs.server.host}:${configs.server.port}`
+    );
+  });
+})();
