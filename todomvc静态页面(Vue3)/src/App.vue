29b41b9e99f74d9540f1afe408b475496abebd0a
qianfengg <fred_gqf0221@163.com>
2022-06-05 17:49:36 +0800

    update static files

---
+++ todomvc静态页面(Vue3)/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+import TodoFooter from './components/TodoFooter.vue'
+</script>
+
+<template>
+  <section class="todoapp">
+    <TodoHeader></TodoHeader>
+    <TodoMain></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
+</template>
+
+<style></style>
---
+++ todomvc静态页面(Vue3)/src/components/TodoMain.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <section class="main">
+    <input id="toggle-all" class="toggle-all" type="checkbox" />
+    <label for="toggle-all">Mark all as complete</label>
+    <ul class="todo-list">
+      <li class="completed">
+        <div class="view">
+          <input class="toggle" type="checkbox" checked />
+          <label>Taste JavaScript</label>
+          <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template" />
+      </li>
+      <li>
+        <div class="view">
+          <input class="toggle" type="checkbox" />
+          <label>Buy a unicorn</label>
+          <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Rule the web" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style lang="less" scoped></style>
